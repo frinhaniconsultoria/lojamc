510924d06a386a7809fba4944c8593eec4936b53
Thiago Frinhani <frinhaniconsultoria@gmail.com>
2020-04-15 21:23:40 -0300

    Clean project

---
+++ src/main/java/br/com/lojamc/LojamcApplication.java
@@ -0,0 +1,13 @@
+package br.com.lojamc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojamcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojamcApplication.class, args);
+	}
+
+}
